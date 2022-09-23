7f75cbc8f3b1e04a073e8531f4ac0c1e1956218e
DaviJust <davijustinocouto@gmail.com>
2022-09-23 15:39:58 -0300

    Trabalho do dia 23 feito

---
+++ HTML e Javascript 23-09/index.js
@@ -0,0 +1,8 @@
+var x = "Não tenho dupla, e nem trio";
+var y = 24;
+
+x = y
+
+console.log("O usuário de nome: " + x + " tem " + y + " Anos");
+typeof x
+typeof y
---
+++ HTML e Javascript 23-09/tiposOperadores.js
@@ -0,0 +1,9 @@
+var cliente = "Alcides Garcia";
+var conta = 67768734;
+var saldo = 322.95;
+var somandoStringNumber = cliente + saldo
+
+console.log("Cliente: " + cliente );
+console.log("conta: " + conta );
+console.log("saldo: " + saldo);
+console.log(somandoStringNumber)
