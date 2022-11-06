5517495c7c1085bd97cd9ee2a9d3284a039ea584
Davi <112717111+DaviJust@users.noreply.github.com>
2022-11-06 17:09:26 -0300

    Novas Aulas

---
+++ 31-10-2022/script/script.js
@@ -0,0 +1,12 @@
+const myH2 = quertSelector('.myh2');  //classe
+const primeiroH = document.quertSelector(".myH2") //classe
+var diversosH = document.querySelectorAll(".myH2") //classe todos os elementos
+var primeiroP = document.querySelector("p") //Tag
+
+var primeiroH1 = document.querySelector("#myH1") //Vai devolver o primeiro ID que tiver
+var primeiroH1 = document.getElementById("myH1") //Vai devolver o primeiro ID que tiver
+
+console.log(primeiroP);
+
+
+var myBody = document.quertSelector('body')
